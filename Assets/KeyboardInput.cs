91890120ac53e557ffbad42914162b2737c0c75b
maxvzd <153630971+maxvzd@users.noreply.github.com>
2024-07-15 22:04:37 +0100

    Basic first person locomotion
    scroll to move faster or slower

--- Assets/KeyboardInput.cs
+++ Assets/KeyboardInput.cs
@@ -1,9 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KeyboardInput : MonoBehaviour
 {
+    public Animator animator;
+    private float _walkSpeed = 1f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,18 @@ public class KeyboardInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float verticalAxis = Input.GetAxis("Vertical");
+        float horizontalAxis = Input.GetAxis("Horizontal");
         
+        _walkSpeed += Input.GetAxis("Mouse ScrollWheel");
+
+        _walkSpeed = Mathf.Clamp(_walkSpeed, 0.5f, 2f);
+
+        verticalAxis *= _walkSpeed;
+        //horizontalAxis *= _walkSpeed;
+        
+        
+        animator.SetFloat("Vertical", verticalAxis);
+        animator.SetFloat("Horizontal", horizontalAxis);
     }
 }
--- Assets/MouseInput.cs
+++ Assets/MouseInput.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class MouseInput : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float sensitivity = 1.0f;
+    public Camera firstPersonCamera;
+    public float maxVerticalAngle;
+
+    private float rotationY = 0.0f;
+    private float rotationX = 0.0f;
+
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rotationY += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        rotationX += Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        rotationX = Mathf.Clamp(rotationX, -maxVerticalAngle, maxVerticalAngle);
+
+        transform.eulerAngles = new Vector3(0, rotationY, 0);
+        firstPersonCamera.transform.eulerAngles = new Vector3(-rotationX, rotationY, 0);
     }
 }
